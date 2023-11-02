c1208a173365e4179b724edc18308b6a8d091e1a
Felician Popa <felicianpopa@yahoo.com>
2023-11-02 13:33:12 +0200

    Create a server using nodejs only

--- logEvents.js
+++ logEvents.js
@@ -7,17 +7,16 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message) => {
+const logEvents = async (message, fileName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-  console.log(logItem);
 
   try {
     if (!fs.existsSync(path.join(__dirname, "logs"))) {
       await fsPromises.mkdir(path.join(__dirname, "logs"));
     }
     await fsPromises.appendFile(
-      path.join(__dirname, "logs", "eventLog.txt"),
+      path.join(__dirname, "logs", fileName),
       logItem
     );
   } catch (error) {
